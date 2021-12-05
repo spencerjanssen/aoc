12d6f7e5386af271e0af37796d7a6d765f292c38
Spencer Janssen <spencerjanssen@gmail.com>
2021-12-05 15:28:24 -0600

    Day 5 part 2

--- src/Y2021/Day5.hs
+++ src/Y2021/Day5.hs
@@ -52,32 +52,67 @@ segments = do
     eof
     pure segs
 
--- >>> expandSegment ((0, 0), (0, 3))
+data SegmentMode
+    = NonDiagonal
+    | IncludeDiagonal
+    deriving (Eq)
+
+-- >>> expandSegment NonDiagonal ((0, 0), (0, 3))
 -- Just [(0,0),(0,1),(0,2),(0,3)]
-expandSegment :: Segment -> Maybe ExpandedSegment
-expandSegment ((x0, y0), (x1, y1))
+-- >>> expandSegment IncludeDiagonal ((0, 0), (3, 3))
+-- Just [(0,0),(1,1),(2,2),(3,3)]
+-- >>> expandSegment IncludeDiagonal ((5, 3), (2, 0))
+-- Just [(5,3),(4,2),(3,1),(2,0)]
+-- >>> expandSegment IncludeDiagonal ((9, 7), (7, 9))
+-- Just [(9,7),(8,8),(7,9)]
+expandSegment :: SegmentMode -> Segment -> Maybe ExpandedSegment
+expandSegment sm ((x0, y0), (x1, y1))
     | x0 == x1 = Just [(x0, y) | y <- [min y0 y1 .. max y0 y1]]
     | y0 == y1 = Just [(x, y0) | x <- [min x0 x1 .. max x0 x1]]
+    | sm == IncludeDiagonal && abs dx == abs dy =
+        Just $
+            zip
+                [x0, x0 + signum dx .. x1]
+                [y0, y0 + signum dy .. y1]
     | otherwise = Nothing
+  where
+    dx = x1 - x0
+    dy = y1 - y0
 
 placeCoord :: Coord -> Placements -> Placements
 placeCoord c = insertWith (+) c 1
+
+occupied :: SegmentMode -> [Segment] -> Int
+occupied sm =
+    length
+        . filter (> 1)
+        . elems
+        . foldl' (flip placeCoord) mempty
+        . concat
+        . mapMaybe (expandSegment sm)
 
 -- >>> part1 parsedExample
 -- 5
 -- >>> part1 parsedProblem
 -- 7380
 part1 :: [Segment] -> Int
-part1 =
-    length
-        . filter (> 1)
-        . elems
-        . foldl' (flip placeCoord) mempty
-        . concat
-        . mapMaybe expandSegment
+part1 = occupied NonDiagonal
+
+-- >>> part2 parsedExample
+-- 12
+-- >>> part2 parsedProblem
+-- 21373
+part2 :: [Segment] -> Int
+part2 = occupied IncludeDiagonal
 
 unit_part1_example :: Assertion
 unit_part1_example = part1 parsedExample @?= 5
 
 unit_part1_problem :: Assertion
 unit_part1_problem = part1 parsedProblem @?= 7380
+
+unit_part2_example :: Assertion
+unit_part2_example = part2 parsedExample @?= 12
+
+unit_part2_problem :: Assertion
+unit_part2_problem = part2 parsedProblem @?= 21373
