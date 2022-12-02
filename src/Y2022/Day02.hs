7525b259b1b7f608138e11adc05cc68a4767d56c
Spencer Janssen <spencerjanssen@users.noreply.github.com>
2022-12-02 15:29:37 +0000

    2022 day 2, part 2

--- src/Y2022/Day02.hs
+++ src/Y2022/Day02.hs
@@ -2,6 +2,7 @@ module Y2022.Day02 where
 
 import Data.FileEmbed (embedFile)
 import MegaParsecUtil
+import Relude.Extra.Map
 import Test.Tasty.HUnit
 import Text.Megaparsec hiding (some)
 import Text.Megaparsec.Char
@@ -21,7 +22,7 @@ parsedProblem :: [(ABC, XYZ)]
 parsedProblem = parseThrow tournament "problem" problem
 
 data RPS = Rock | Paper | Scissors
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 data ABC = A | B | C
     deriving (Show)
@@ -63,8 +64,14 @@ decodeXYZ = \case
     Y -> Paper
     Z -> Scissors
 
+decodeXYZ' :: XYZ -> Winner
+decodeXYZ' = \case
+    X -> Opponent
+    Y -> Draw
+    Z -> You
+
 data Winner = Opponent | You | Draw
-    deriving (Show)
+    deriving (Show, Eq, Ord)
 
 -- >>> liftA2 (\o y -> (o, y, result o y)) [Rock, Paper, Scissors] [Rock, Paper, Scissors]
 -- [(Rock,Rock,Draw),(Rock,Paper,Opponent),(Rock,Scissors,You),(Paper,Rock,Opponent),(Paper,Paper,Draw),(Paper,Scissors,You),(Scissors,Rock,You),(Scissors,Paper,Opponent),(Scissors,Scissors,Draw)]
@@ -76,6 +83,12 @@ result opp you
         (Paper, Rock) -> Opponent
         (Scissors, Paper) -> Opponent
         _ -> You
+
+reverseResult :: RPS -> Winner -> RPS
+reverseResult opp res = fromMaybe (error "missing result, impossible") $ lookup (opp, res) reverseResultMap
+
+reverseResultMap :: Map (RPS, Winner) RPS
+reverseResultMap = fromList [((opp, result opp you), you) | opp <- [Rock, Paper, Scissors], you <- [Rock, Paper, Scissors]]
 
 score :: RPS -> RPS -> Int
 score opp you = scoreYou + scoreResult
@@ -94,8 +107,26 @@ score opp you = scoreYou + scoreResult
 part1 :: [(ABC, XYZ)] -> Int
 part1 = sum . map (\(x, a) -> score (decodeABC x) (decodeXYZ a))
 
+-- >>> part2 parsedExample
+-- 12
+part2 :: [(ABC, XYZ)] -> Int
+part2 =
+    sum
+        . map
+            ( \(a, x) ->
+                let opp = decodeABC a
+                    you = reverseResult opp (decodeXYZ' x)
+                 in score opp you
+            )
+
 unit_part1_example :: Assertion
 unit_part1_example = part1 parsedExample @?= 15
 
 unit_part1_problem :: Assertion
 unit_part1_problem = part1 parsedProblem @?= 12855
+
+unit_part2_example :: Assertion
+unit_part2_example = part2 parsedExample @?= 12
+
+unit_part2_problem :: Assertion
+unit_part2_problem = part2 parsedProblem @?= 13726
