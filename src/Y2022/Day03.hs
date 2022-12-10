ddd3f598e3b2d917a16d0721cd9243b2502d6084
Spencer Janssen <spencerjanssen@users.noreply.github.com>
2022-12-10 06:52:30 +0000

    2022 day 3, part 2

--- src/Y2022/Day03.hs
+++ src/Y2022/Day03.hs
@@ -15,7 +15,7 @@ puzzle =
         { year = "2022"
         , day = "03"
         , parser = rucksack
-        , parts = [Part part1 157 8088]
+        , parts = [Part part1 157 8088, Part part2 70 2522]
         }
 
 test_ :: TestTree
@@ -28,6 +28,11 @@ rucksack = endBy (many asciiAlphaChar) eol <* eof
 -- ("ab","cd")
 compartments :: [Char] -> ([Char], [Char])
 compartments xs = splitAt (length xs `div` 2) xs
+
+groups :: Int -> [a] -> [[a]]
+groups _ [] = []
+groups n x = case splitAt n x of
+    (l, r) -> l : groups n r
 
 score :: Char -> Int
 score x
@@ -45,3 +50,12 @@ part1Line x = common
   where
     (l, r) = compartments x
     common = foldMap (Sum . score) $ Set.intersection (fromList l) (fromList r)
+
+part2 :: [[Char]] -> Int
+part2 = getSum . foldMap part2Group . groups 3
+
+part2Group :: [[Char]] -> Sum Int
+part2Group = foldMap (foldMap (Sum . score)) . badge
+
+badge :: [[Char]] -> Maybe (Set Char)
+badge = viaNonEmpty (\(x :| xs) -> foldl' Set.intersection x xs) . map fromList
