f0659b9b949ad0e6210cce929b3b617f6ebca14c
Spencer Janssen <spencerjanssen@users.noreply.github.com>
2022-12-22 06:34:09 +0000

    2022, day 21, part 2

--- src/Y2022/Day21.hs
+++ src/Y2022/Day21.hs
@@ -16,23 +16,26 @@ puzzle =
         { year = "2022"
         , day = "21"
         , parser = statements
-        , parts = [Part (Map.lookup (MonkeyName "root") . solve) (Just 152) (Just 72664227897438)]
+        , parts =
+            [ Part (Map.lookup "root" . solve) (Just 152) (Just 72664227897438)
+            , Part (findZero . equation) 301 3916491093817
+            ]
         }
 
 test_ :: TestTree
 test_ = tests puzzle
 
 data O = Plus | Sub | Mul | Div
-    deriving (Show)
+    deriving (Show, Eq)
 
 newtype MonkeyName = MonkeyName Text
-    deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord, IsString)
 
 data Expr a
-    = C Int
+    = C Rational
     | Pure a
     | Oper (Expr a) O (Expr a)
-    deriving (Functor, Show)
+    deriving (Functor, Show, Eq)
 
 joinExpr :: Expr (Expr a) -> Expr a
 joinExpr = \case
@@ -57,7 +60,7 @@ monkeyName :: Parsec Void Text MonkeyName
 monkeyName = MonkeyName <$> asciiAlpha
 
 expr :: Parsec Void Text (Expr MonkeyName)
-expr = C <$> int <|> Oper <$> (Pure <$> monkeyName) <*> oper <*> (Pure <$> monkeyName)
+expr = C . fromIntegral <$> int <|> Oper <$> (Pure <$> monkeyName) <*> oper <*> (Pure <$> monkeyName)
 
 oper :: Parsec Void Text O
 oper =
@@ -68,20 +71,84 @@ oper =
         , " / " $> Div
         ]
 
-eval :: Expr Int -> Int
-eval = \case
-    C n -> n
-    Pure n -> n
-    Oper x o y -> operFn o (eval x) (eval y)
+efold :: (Rational -> b) -> (a -> b) -> (O -> b -> b -> b) -> Expr a -> b
+efold c p op = go
+  where
+    go = \case
+        C n -> c n
+        Pure x -> p x
+        Oper x o y -> op o (go x) (go y)
 
-operFn :: O -> Int -> Int -> Int
+eval :: Expr Rational -> Rational
+eval = efold id id operFn
+
+operFn :: O -> Rational -> Rational -> Rational
 operFn = \case
     Plus -> (+)
     Sub -> (-)
     Mul -> (*)
-    Div -> div
+    Div -> (/)
 
-solve :: [(MonkeyName, Expr MonkeyName)] -> Map MonkeyName Int
+solve :: [(MonkeyName, Expr MonkeyName)] -> Map MonkeyName Rational
 solve es = m
   where
     m = Map.fromList $ map (fmap (\e -> eval $ (m Map.!) <$> e)) es
+
+data Humn = Humn
+    deriving (Show, Eq)
+
+equation :: [(MonkeyName, Expr MonkeyName)] -> Expr Humn
+equation es = case m Map.! "root" of
+    Oper x _ y -> Oper x Sub y
+    _ -> error "root is not an operator"
+  where
+    m = Map.fromList $ map f es
+    f (mn, e) =
+        ( mn
+        , case mn of
+            "humn" -> Pure Humn
+            _ -> (m Map.!) =<< e
+        )
+
+simplify :: Expr a -> Expr a
+simplify = efold C Pure f
+  where
+    f o (C x) (C y) = C $ operFn o x y
+    f o x y = Oper x o y
+
+fixpoint :: Eq a => (a -> a) -> a -> a
+fixpoint f x
+    | x' == f x = x
+    | otherwise = fixpoint f x'
+  where
+    x' = f x
+
+derivative :: Expr a -> Expr a
+derivative = \case
+    Oper x Plus y -> Oper (derivative x) Plus (derivative y)
+    Oper x Sub y -> Oper (derivative x) Sub (derivative y)
+    Oper f Mul g ->
+        let f' = derivative f
+            g' = derivative g
+         in (f' `emul` g) `eplus` (f `emul` g')
+    Oper f Div g ->
+        let f' = derivative f
+            g' = derivative g
+         in ((f' `emul` g) `eplus` (f `emul` g')) `ediv` (g `emul` g)
+    Pure x -> Pure x
+    C _ -> C 0
+  where
+    emul f = Oper f Mul
+    eplus f = Oper f Plus
+    ediv f = Oper f Div
+
+findZero :: Expr Humn -> Rational
+findZero ex = go 1
+  where
+    f x = eval $ ex <&> \Humn -> x
+    dexp = derivative ex
+    f' x = eval $ dexp <&> \Humn -> x
+    approx x = x - f x / f' x
+    go x
+        | f x == 0 = x
+        | otherwise = go $ approx x
