46679ed938f7ced07463520ce171937d1480a14d
Spencer Janssen <spencerjanssen@gmail.com>
2023-01-27 20:32:28 +0000

    2022, day 15, part 2

--- src/Y2022/Day15.hs
+++ src/Y2022/Day15.hs
@@ -3,6 +3,7 @@ module Y2022.Day15 where
 import AocUtil
 import MegaParsecUtil
 import Test.Tasty
+import Test.Tasty.HUnit
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Prelude hiding (many, round, some)
@@ -18,6 +19,18 @@ puzzle =
 
 test_ :: TestTree
 test_ = tests puzzle
+
+test_part2 :: TestTree
+test_part2 =
+    testGroup
+        "Part 2"
+        [ testCase "Example" $ do
+            i <- puzzleInput puzzle Example
+            map tuningFrequency (search (0, 20) i) @?= [56_000_011]
+        , testCase "Problem" $ do
+            i <- puzzleInput puzzle Problem
+            map tuningFrequency (search (0, 4_000_000) i) @?= [13615843289729]
+        ]
 
 type Point = (Int, Int)
 
@@ -40,40 +53,52 @@ manhattan (x0, y0) (x1, y1) = abs (x1 - x0) + abs (y1 - y0)
 -- >>> beaconless 10 ((8, 7), (2, 10))
 -- Just (3,14)
 beaconless :: Int -> (Point, Point) -> Maybe Interval
-beaconless y (ps@(sx, sy), pb@(bx, by))
+beaconless y (ps@(sx, sy), pb)
     | dy > d = Nothing
-    | y == by = excludeExtreme bx i
     | otherwise = Just i
   where
     d = manhattan ps pb
     dy = abs (sy - y)
     i = (sx - d + dy, sx + d - dy)
 
-excludeExtreme :: Int -> Interval -> Maybe Interval
-excludeExtreme p (x, y)
-    | p == x && p == y = Nothing
-    | p == x = Just (succ x, y)
-    | p == y = Just (x, pred y)
-    | otherwise = Just (x, y)
-
 type IntervalSet = [(Int, Int)]
 
 inInterval :: Int -> Interval -> Bool
 inInterval n (u, v) = n >= u && n <= v
 
-union :: Interval -> Interval -> Either (Interval, Interval) Interval
+union :: Interval -> Interval -> Maybe Interval
 union x@(x0, x1) y@(y0, y1)
-    | inInterval x0 y || inInterval y0 x = Right (min x0 y0, max x1 y1)
-    | otherwise = Left (x, y)
+    | or [succ x1 == y0, succ y1 == x0, inInterval x0 y, inInterval y0 x] = Just (min x0 y0, max x1 y1)
+    | otherwise = Nothing
 
 magnitude :: Interval -> Sum Int
 magnitude (x, y) = Sum $ y - x + 1
 
 intervalSet :: [Interval] -> [Interval]
-intervalSet [] = []
-intervalSet (x : xs) = case partitionWith (union x) xs of
-    (_, []) -> x : intervalSet xs
-    (ns, o : os) -> intervalSet $ (o : os) <> map snd ns
+intervalSet = foldl' (flip insert) []
+  where
+    insert x [] = [x]
+    insert x (y : ys) = case x `union` y of
+        Just z -> insert z ys
+        Nothing
+            | x > y -> y : insert x ys
+            | otherwise -> x : y : ys
+
+noBeacons :: Int -> [(Point, Point)] -> [Interval]
+noBeacons y = intervalSet . mapMaybe (beaconless y)
 
 part1 :: Int -> [(Point, Point)] -> Sum Int
-part1 y = foldMap magnitude . intervalSet . intervalSet . mapMaybe (beaconless y)
+part1 y ps = foldMap magnitude (noBeacons y ps) <> Sum (negate $ length $ ordNub $ filter ((== y) . snd) $ map snd ps)
+
+search :: Interval -> [(Point, Point)] -> [(Int, Int)]
+search (lo, hi) ps =
+    ordNub $
+        [ (x, y)
+        | y <- [lo .. hi]
+        , (x0, x1) <- intervalSet $ mapMaybe (beaconless y) ps
+        , x <- [pred x0, succ x1]
+        , x > lo && x < hi
+        ]
+
+tuningFrequency :: Point -> Int
+tuningFrequency (x, y) = x * 4_000_000 + y
