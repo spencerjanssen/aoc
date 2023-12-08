559e13648966b2129b6a129678a0ef49826c1595
Spencer Janssen <spencerjanssen@users.noreply.github.com>
2023-12-08 02:47:21 +0000

    2023, day 3, part 2

--- src/Y2023/Day03.hs
+++ src/Y2023/Day03.hs
@@ -1,6 +1,8 @@
 module Y2023.Day03 where
 
 import AocUtil
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
 import MegaParsecUtil
 import Relude.Extra.Map
 import Test.Tasty
@@ -16,6 +18,7 @@ puzzle =
         , parser = schematic
         , parts =
             [ Part part1 4361 543867
+            , Part part2 467835 79613331
             ]
         }
 
@@ -23,7 +26,7 @@ test_ :: TestTree
 test_ = tests puzzle
 
 data PartPos = PartPos {lineNo, startCol, endCol :: Int}
-    deriving (Show)
+    deriving (Show, Eq, Ord)
 
 data Schematic = Schematic
     { partNumbers :: [(PartPos, Int)]
@@ -65,3 +68,22 @@ eligible s = [pn | (pp, pn) <- s.partNumbers, nearSymbol pp]
 
 near :: PartPos -> [(Int, Int)]
 near pp = [(ln, col) | ln <- [pred pp.lineNo .. succ pp.lineNo], col <- [pred pp.startCol .. succ pp.endCol]]
+
+part2 :: Schematic -> Int
+part2 = sum . map (product . map snd . Set.toList) . allGears
+
+allGears :: Schematic -> [Set (PartPos, Int)]
+allGears = filter isGear . toList . nearGears
+
+isGear :: (Ord a) => Set a -> Bool
+isGear = (2 ==) . size
+
+nearGears :: Schematic -> Map (Int, Int) (Set (PartPos, Int))
+nearGears s =
+    Map.fromListWith
+        (<>)
+        [ (rc, one (pp, pn))
+        | (pp, pn) <- s.partNumbers
+        , rc <- near pp
+        , '*' <- maybeToList $ lookup rc s.symbols
+        ]
